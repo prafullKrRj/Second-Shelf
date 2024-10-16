95e8c11874aacc850e50b96b791f6dbddf331b3f
prafullKrRj <prafullkumar384@gmail.com>
2024-10-16 10:38:36 +0530

    docker

--- src/test/java/com/prafull/secondshelf/SecondShelfApplicationTests.java
+++ src/test/java/com/prafull/secondshelf/SecondShelfApplicationTests.java
@@ -2,8 +2,14 @@ package com.prafull.secondshelf;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@TestPropertySource(properties = {
+        "spring.datasource.url=${JDBC_DATABASE_URL}",
+        "spring.datasource.username=${JDBC_DATABASE_USERNAME}",
+        "spring.datasource.password=${JDBC_DATABASE_PASSWORD}"
+})
 class SecondShelfApplicationTests {
 
     @Test
